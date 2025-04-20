d2b21fee15801ca66bfab387cc2d281ecef17e11
Felipe Ribeiro <felipe_1540@outlook.com>
2025-04-20 11:06:38 -0300

    Ajustando documentacao com swagger ui

--- src/main/java/com/example/controller/VotacaoController.java
+++ src/main/java/com/example/controller/VotacaoController.java
@@ -4,6 +4,8 @@ import com.example.dto.PautaDTO;
 import com.example.dto.VotoDTO;
 import com.example.model.Pauta;
 import com.example.service.VotacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,20 +15,24 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Tag(name = "Votação", description = "Endpoints para votação em pautas")
 public class VotacaoController {
 
     private final VotacaoService votacaoService;
 
+    @Operation(summary = "Cadastrar uma pauta")
     @PostMapping("/pauta")
     public Pauta cadastrarPauta(@RequestBody @Valid PautaDTO pautaDto) {
         return votacaoService.cadastrarPauta(pautaDto);
     }
 
+    @Operation(summary = "Cadastrar um voto")
     @PostMapping("/votar")
     public void cadastrarVoto(@RequestBody @Valid VotoDTO votoDto) {
         votacaoService.cadastrarVoto(votoDto);
     }
 
+    @Operation(summary = "Buscar a contagem de votos de determinada pauta. api/contagem?pautaId=ID")
     @GetMapping("/contagem")
     public String contarVotos(@RequestParam Long pautaId) {
         return votacaoService.contagemDeVotos(pautaId);
